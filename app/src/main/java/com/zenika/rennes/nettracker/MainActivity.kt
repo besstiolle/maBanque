2cd4912ed9d18199c1dfb2dbec4ec8a8e92fc2d5
Danezis <kdanezis@gmail.com>
2020-11-27 21:00:04 +0100

    :sparkles: add button for refresh

--- app/src/main/java/com/zenika/rennes/nettracker/MainActivity.kt
+++ app/src/main/java/com/zenika/rennes/nettracker/MainActivity.kt
@@ -3,6 +3,7 @@ package com.zenika.rennes.nettracker
 import android.os.Build
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -18,6 +19,13 @@ class MainActivity : AppCompatActivity() {
         initNetMeter()
 
         checkIsOnline()
+
+        //Plug button click event
+        val button: Button = findViewById(R.id.button_refresh)
+        button.setOnClickListener {
+            checkIsOnline()
+        }
+
     }
 
     private fun initNetMeter(){
