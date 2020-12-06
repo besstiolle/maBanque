c9cb3e162ae2d1fb6acd561ff48d7dd8116baa2c
Danezis <kdanezis@gmail.com>
2020-12-06 22:28:29 +0100

    :sparkles: connecting splash screen to version Activity

--- app/src/main/java/com/zenika/rennes/mbel/VersionActivity.kt
+++ app/src/main/java/com/zenika/rennes/mbel/VersionActivity.kt
@@ -4,6 +4,7 @@ import android.app.AlertDialog
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -14,26 +15,39 @@ class VersionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        showDialog()
+        // Question if < 17
+        if(Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            showDialog()
+        } else {
+            gotoMainActivity()
+        }
+
     }
 
     private fun showDialog() {
         val builder = AlertDialog.Builder(this)
         builder.setTitle("Version d'Android ancienne")
-        builder.setMessage("Votre smartphone est ancien. Pour bénéficier de toutes les fonctionnalités de l'application nous vous conseillons d'aller sur notre site internet. Souhaitez vous y accéder de suite ?")
-        builder.setPositiveButton("oui") { _, _ -> gotoWebsite()}
-        builder.setNeutralButton("non") { _, _ -> gotoMainActivity()}
+        builder.setMessage("Votre smartphone est ancien. Pour bénéficier de toutes les fonctionnalités " +
+                "de l'application nous vous conseillons d'aller sur notre site internet. \n" +
+                "Aller sur notre site ?")
+        builder.setPositiveButton("oui") { _, _ ->
+            toast("ok, go to website")
+            gotoWebsite()
+        }
+        builder.setNeutralButton("non") { _, _ ->
+            toast("ok, we continue")
+            gotoMainActivity()
+        }
+        builder.setCancelable(false)
         builder.show()
     }
 
     private fun gotoWebsite(){
-        toast("ok, go to website")
         startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("http://www.google.com")))
         finishAffinity()
     }
 
     private fun gotoMainActivity(){
-        toast("ok, we continue")
         val intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
         finish()
